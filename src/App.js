09dea4f9575b785ec3b9bc54dfee38a985acb5b9
khouloudhaddad <khouloud.haddad@gmail.com>
2023-04-10 15:10:45 +0100

    Adding Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </>
   );
 }
